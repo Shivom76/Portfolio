06fcedfc48bd1d2e0925a4225412704ab2776377
Shivom76 <shivompaul76@gmail.com>
2026-01-16 19:38:53 +0530

    updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import "./css/Footer.css"
 export default function Footer(){
     return(
         <div className="bg-black text-white text-center w-auto bottom-0 p-4">
-            <h2 className="text-center mb-1">Developer page</h2>
-            <p className="text-xs mb-4">Something written here</p>
+            <h2 className="text-center mb-1">Digital design by <b>Shivom</b></h2>
+            <p className="text-xs mb-4">Thanks for visiting &hearts; </p>
             <hr className="w-2/3 mx-auto " />
             <div className="logos mt-3">
                 <a href="https://www.linkedin.com/in/shivom-paul-53072222b/" className="px-1"><i className="fa-brands fa-linkedin opacity-70"></i></a>  
