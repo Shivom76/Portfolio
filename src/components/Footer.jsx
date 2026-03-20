550fc1c3690b10f3ca1aae1a041cc94f929846fc
Shivom76 <shivompaul76@gmail.com>
2026-03-20 15:03:37 +0530

    addgin target blank on anchored pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ export default function Footer(){
             <p className="text-xs mb-4">Thanks for visiting &hearts; </p>
             <hr className="w-2/3 mx-auto " />
             <div className="logos mt-3">
-                <a href="https://www.linkedin.com/in/shivom-paul-53072222b/" className="px-1"><i className="fa-brands fa-linkedin opacity-70"></i></a>  
-                <a href="https://github.com/Shivom76" className="px-1"><i className="fa-brands fa-github opacity-70"></i></a>
-                <a href="#" className="px-1"><i className="fa-brands fa-x-twitter opacity-70"></i></a>
-                <a href="https://www.instagram.com/_.abyssal_mist/" className="px-1"><i className="fa-brands fa-instagram opacity-70"></i></a>
+                <a href="https://www.linkedin.com/in/shivom-paul-53072222b/"  target="_blank" rel="noreferrer" className="px-1"><i className="fa-brands fa-linkedin opacity-70"></i></a>  
+                <a href="https://github.com/Shivom76" className="px-1" target="_blank" rel="noreferrer" ><i className="fa-brands fa-github opacity-70"></i></a>
+                <a href="#" className="px-1" target="_blank" rel="noreferrer" ><i className="fa-brands fa-x-twitter opacity-70"></i></a>
+                <a href="https://www.instagram.com/_.abyssal_mist/" target="_blank" rel="noreferrer" className="px-1"><i className="fa-brands fa-instagram opacity-70"></i></a>
             </div>
         </div>
     )
